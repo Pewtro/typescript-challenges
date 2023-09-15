5799e6fbd6edc96f655e5855d68320db36c7e40c
Putro <laustsf@gmail.com>
2023-09-15 18:21:23 +0200

    Solve 00011-easy-tuple-to-object

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,16 @@ module.exports = {
   },
   rules: {
     //cases in typescript-challenges are always unused
-    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: 'cases' }],
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: 'cases|error' }],
     //ts-expect-error is often used in typescript-challenges
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-expect-error': false }],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          '{}': false,
+        },
+      },
+    ],
   },
 };
